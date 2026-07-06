8ada0ecb2b8c4822579beee94aa43070448e8220
Youdaox <youdao005@gmail.com>
2026-07-06 13:57:13 +1200

    exercise 7, added ScrollView component to appbar

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 import theme from '../theme';
@@ -17,8 +17,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
+      <ScrollView horizontal>
         <AppBarTab text="Repositories" linkTo="/" />
         <AppBarTab text="Sign In" linkTo="/signin" />
+      </ScrollView>
     </View>
   );  
 };
