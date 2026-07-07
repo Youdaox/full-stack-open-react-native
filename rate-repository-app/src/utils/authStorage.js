76abe7de6bdea5a3cedbc373760c9da08a360f44
Youdaox <youdao005@gmail.com>
2026-07-07 19:58:06 +1200

    Storing the access token using AsyncStorage, exercise 14

---
+++ rate-repository-app/src/utils/authStorage.js
@@ -0,0 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class AuthStorage {
+  constructor(namespace = 'auth') {
+    this.namespace = namespace;
+  }
+
+  async getAccessToken() {
+    const accessToken = await AsyncStorage.getItem(`${this.namespace}:authToken`)
+    return accessToken
+  }
+
+  async setAccessToken(accessToken) {
+    await AsyncStorage.setItem(`${this.namespace}:authToken`)
+  }
+
+  async removeAccessToken() {
+    await AsyncStorage.removeItem(`${this.namespace}:authToken`)
+  }
+}
+
+export default AuthStorage;
