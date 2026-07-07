8ac884749926c8088a0bb8be6e9d6abadb01d3a9
Youdaox <youdao005@gmail.com>
2026-07-07 13:07:25 +1200

    Fetching repositories with Apollo Client, exercise 11

---
+++ rate-repository-app/src/graphql/queries.js
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+
+export const GET_REPOSITORIES = gql`
+  query {
+    repositories {
+      totalCount
+      pageInfo {
+        hasPreviousPage
+        hasNextPage
+        startCursor
+        endCursor
+      }
+      edges {
+        cursor
+        node {
+          id
+          name
+          ownerName
+          createdAt
+          fullName
+          reviewCount
+          ratingAverage
+          forksCount
+          stargazersCount
+          description
+          language
+          ownerAvatarUrl
+        }
+      }
+    }
+  }
+`;
